a9b64b4766007358dda4dd08cc62af8d8270e288
Nilu <21nilu@mail.ru>
2022-12-27 17:29:43 +0300

    работа с функциями

--- Program.cs
+++ Program.cs
@@ -2,3 +2,33 @@
 string[] array = new string[5] {"123", "Hello", "Russia", "-2", ": )"};
 string[] text = new string[array.Length];
 int n = 3;
+
+
+
+void Find(string[] array, string[] text)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            text[count] = array[i];
+            count++;
+        }
+        else
+        {
+            text[count] = string.Empty;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+
